40f3cc548b8f97434c250de071d3c7a41d0830f4
huruijie-steam <3099566364@qq.com>
2026-08-05 11:07:22 +0800

    初始提交:HotTopic 热点话题项目

---
+++ src/test/java/com/yupi/hottopic/HotTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.hottopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotTopicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
